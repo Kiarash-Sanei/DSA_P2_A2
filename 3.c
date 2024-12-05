ff97b19b607ab7541dcdd0a7645321aa115434cb
Kiarash Sanei <kiarashsanei5@gmail.com>
2024-12-05 18:19:29 +0330

    Third attempt is done

--- 3.c
+++ 3.c
@@ -2,13 +2,101 @@
 #include <math.h>
 #include <stdlib.h>
 
-typedef struct list 
+typedef struct list
 {
-    int* leftTowers;
+    int *leftTowers;
     int leftSize;
-    int* rightTowers;
+    int *rightTowers;
     int rightSize;
 } list;
+
+void leftCalculator(list *possible, int towers[][3], int peopleCount)
+{
+    possible[0].leftSize = 0;
+    possible[0].leftTowers = NULL;
+    for (int i = 1; i < peopleCount; i++)
+    {
+        if (possible[i - 1].leftSize != 0)
+        {
+            possible[i].leftSize = 0;
+            possible[i].leftTowers = (int *)malloc((possible[i - 1].leftSize + 1) * sizeof(int));
+            for (int j = 0; j < possible[i - 1].leftSize; j++)
+            {
+                if (towers[possible[i - 1].leftTowers[j]][0] >= towers[i - 1][0])
+                {
+                    possible[i].leftTowers[possible[i].leftSize] = possible[i - 1].leftTowers[j];
+                    possible[i].leftSize++;
+                }
+            }
+            possible[i].leftTowers[possible[i].leftSize] = i - 1;
+            possible[i].leftSize++;
+        }
+        else
+        {
+            possible[i].leftSize = 1;
+            possible[i].leftTowers = (int *)malloc(sizeof(int));
+            possible[i].leftTowers[0] = i - 1;
+        }
+    }
+}
+
+void rightCalculator(list *possible, int towers[][3], int peopleCount)
+{
+    possible[peopleCount - 1].rightSize = 0;
+    possible[peopleCount - 1].rightTowers = NULL;
+    for (int i = peopleCount - 2; i >= 0; i--)
+    {
+        if (possible[i + 1].rightSize != 0)
+        {
+            possible[i].rightSize = 0;
+            possible[i].rightTowers = (int *)malloc((possible[i + 1].rightSize + 1) * sizeof(int));
+            for (int j = 0; j < possible[i + 1].rightSize; j++)
+            {
+                if (towers[possible[i + 1].rightTowers[j]][0] >= towers[i + 1][0])
+                {
+                    possible[i].rightTowers[possible[i].rightSize] = possible[i + 1].rightTowers[j];
+                    possible[i].rightSize++;
+                }
+            }
+            possible[i].rightTowers[possible[i].rightSize] = i + 1;
+            possible[i].rightSize++;
+        }
+        else
+        {
+            possible[i].rightSize = 1;
+            possible[i].rightTowers = (int *)malloc(sizeof(int));
+            possible[i].rightTowers[0] = i + 1;
+        }
+    }
+}
+
+void printer(list *possible, int towers[][3], int peopleCount)
+{
+    for (int i = 0; i < peopleCount; i++)
+    {
+        int hit = 0;
+        for (int j = 0; j < possible[i].leftSize; j++)
+        {
+            hit += towers[possible[i].leftTowers[j]][2];
+        }
+        for (int j = 0; j < possible[i].rightSize; j++)
+        {
+            hit += towers[possible[i].rightTowers[j]][2];
+        }
+        double result = ((double)towers[i][1]) / ((double)hit);
+        printf("%.0lf ", ceil(result));
+    }
+    printf("\n");
+}
+
+void freeMemory(list *array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        free(array[i].leftTowers);
+        free(array[i].rightTowers);
+    }
+}
 
 int main()
 {
@@ -18,83 +106,21 @@ int main()
     {
         int peopleCount;
         scanf("%d", &peopleCount);
+
         int towers[peopleCount][3];
         for (int j = 0; j < peopleCount; j++)
         {
             scanf("%d %d %d", towers[j], towers[j] + 1, towers[j] + 2);
         }
+
         list possible[peopleCount];
-        possible[0].leftSize = 0;
-        possible[0].leftTowers = NULL;
-        for (int j = 1; j < peopleCount; j++)
-        {
-            if (possible[j - 1].leftSize != 0)
-            {
-                possible[j].leftSize = 0;
-                possible[j].leftTowers = (int*) malloc((possible[j - 1].leftSize + 1) * sizeof(int));
-                for (int k = 0; k < possible[j - 1].leftSize; k++)
-                {
-                    if (possible[j - 1].leftTowers[k] >= towers[j - 1][0])
-                    {
-                        possible[j].leftTowers[possible[j].leftSize] = possible[j - 1].leftTowers[k];
-                        possible[j].leftSize++;
-                    }
-                }
-                possible[j].leftTowers[possible[j].leftSize] = j - 1;
-                possible[j].leftSize++;
-            }
-            else 
-            {
-                possible[j].leftSize = 1;
-                possible[j].leftTowers = (int*) malloc(sizeof(int));
-                possible[j].leftTowers[0] = j - 1;
-            }
-        }
-        possible[peopleCount - 1].rightSize = 0;
-        possible[peopleCount - 1].rightTowers = NULL;
-        for (int j = peopleCount - 2; j >= 0; j--)
-        {
-            if (possible[j + 1].rightSize != 0)
-            {
-                possible[j].rightSize = 0;
-                possible[j].rightTowers = (int*) malloc((possible[j + 1].rightSize + 1) * sizeof(int));
-                for (int k = 0; k < possible[j + 1].rightSize; k++)
-                {
-                    if (possible[j + 1].rightTowers[k] >= towers[j + 1][0])
-                    {
-                        possible[j].rightTowers[possible[j].rightSize] = possible[j - 1].rightTowers[k];
-                        possible[j].rightSize++;
-                    }
-                }
-                possible[j].rightTowers[possible[j].rightSize] = j - 1;
-                possible[j].rightSize++;
-            }
-            else 
-            {
-                possible[j].rightSize = 1;
-                possible[j].rightTowers = (int*) malloc(sizeof(int));
-                possible[j].rightTowers[0] = j - 1;
-            }
-        }
-        for (int j = 0; j < peopleCount; j++)
-        {
-            int hit = 0;
-            for (int k = 0; k < possible[j].leftSize; k++)
-            {
-                hit += towers[possible[j].leftTowers[k]][2];
-            }
-            for (int k = 0; k < possible[j].rightSize; k++)
-            {
-                hit += towers[possible[j].rightTowers[k]][2];
-            }
-            double result = ((double) towers[j][1]) / ((double) hit);
-            printf("%.0lf ", ceil(result));
-        }
-        for (int j = 0; j < peopleCount; j++)
-        {
-            free(possible[j].leftTowers);
-            free(possible[j].rightTowers);
-        }
-        printf("\n");
+
+        leftCalculator(possible, towers, peopleCount);
+
+        rightCalculator(possible, towers, peopleCount);
+
+        printer(possible, towers, peopleCount);
+
+        freeMemory(possible, peopleCount);
     }
 }
